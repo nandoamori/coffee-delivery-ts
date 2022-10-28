b8163ef8373816aba8ad981129c593adbd45e194
Fernando Ribeiro de Amorim <nandoamori@yahoo.com.br>
2022-10-28 15:16:36 -0300

    :recycle: refactor: refatorando

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,13 +1,13 @@
 import { HomeContainer } from './styles'
 
 import { Banner } from './components/banner'
-import { CoffeeList } from './components'
+import { CoffeeList, CoffeeProps } from './components'
 
-export function Home() {
+export function Home({ ...props }: CoffeeProps) {
   return (
     <HomeContainer>
       <Banner />
-      <CoffeeList />
+      <CoffeeList {...props} />
     </HomeContainer>
   )
 }
